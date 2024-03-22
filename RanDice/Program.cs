869f45db0ccbb09fb0f013ef0fefd01b850aa40d
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-21 21:06:37 -0400

    Exercício 6 concluido

---
+++ RanDice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach ();
+        }
+    }
+}
