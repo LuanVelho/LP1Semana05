d534fe010a48524fff38d18a4628a6d44aba34e4
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-22 15:47:42 -0400

    Exercício 10 concluído

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,33 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int enemyNum = int.Parse(args[0]);
+
+            Enemy[] enemyArray = new Enemy[enemyNum];
+
+            for (int i = 1; i <= enemyNum; i++)
+            {
+                Console.Write($"Nome do inimigo {i}: ");
+                string name = Console.ReadLine();
+                Enemy newEnemy = new Enemy(name);
+                enemyArray[i-1] = newEnemy;
+            }
+
+            foreach (Enemy guy in enemyArray)
+            {
+                guy.TakeDamage(50);
+                guy.PickupPowerUp(PowerUp.Shield, 25);
+                guy.PickupPowerUp(PowerUp.Health, 25);
+                string name = guy.GetName();
+                float health = guy.GetHealth();
+                float shield = guy.GetShield();
+
+                Console.WriteLine($"{name} {health} {shield}");
+            }
+            
+            float pwpc = enemyArray[0].GetPowerUpCount();
+
+            Console.WriteLine(pwpc);
         }
     }
 }
